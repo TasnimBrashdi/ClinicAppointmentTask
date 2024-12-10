fab11e23f153fb678ae6cc457aa13d82c09d817e
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-10 17:13:50 +0400

    Add Model Clinic

---
+++ ClinicAppointmentTask/Models/Clinic.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicAppointmentTask.Models
+{
+    public class Clinic
+    {
+        [Key]
+        public int CID { get; set; }
+
+        [Required]
+
+        public string Specialization { get; set; }
+
+        [Required]
+
+        public int NoOfSlots { get; set; }
+    }
+}
