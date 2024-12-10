89dde26f3f3580a52532638bcda2b582b3021aee
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-10 17:21:30 +0400

    add Model booking

---
+++ ClinicAppointmentTask/Models/Booking.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicAppointmentTask.Models
+{
+    [PrimaryKey(nameof(PatientID), nameof(ClinicId))]
+    public class Booking
+    {
+        [ForeignKey("PId")]
+        public int PatientID { get; set; }
+        [JsonIgnore]
+        public virtual Patient Patient { get; set; }
+        [ForeignKey("CId")]
+        public int ClinicId { get; set; }
+        [JsonIgnore]
+        public virtual Clinic Clinic { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        public int Slots_Number { get; set; } = 0;
+    }
+}
