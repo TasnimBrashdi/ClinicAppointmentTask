6854200b79e33d3f013d61dd29c6e1067ded336e
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-10 17:11:28 +0400

    Add Model Patient

--- ClinicAppointmentTask/Models/Patient.cs
+++ ClinicAppointmentTask/Models/Patient.cs
@@ -1,6 +1,28 @@
-﻿namespace ClinicAppointmentTask.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClinicAppointmentTask.Models
 {
     public class Patient
     {
+        public enum GENDER
+        {
+            Male,
+            Female
+        }
+        [Key]
+
+        public int PID { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Name { get; set; }
+
+        [Required]
+
+        public int Age { get; set; }
+
+        [Required]
+
+        public GENDER gender { get; set; }
     }
 }
