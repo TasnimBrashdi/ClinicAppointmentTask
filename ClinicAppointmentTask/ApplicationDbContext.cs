731289a63e391ee0ca40021c88124bc839111297
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-11 08:41:31 +0400

    Add ApplicationDbContext

---
+++ ClinicAppointmentTask/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ClinicAppointmentTask.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicAppointmentTask
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Patient> Patients { get; set; }
+    }
+}
