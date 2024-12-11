67dfd33f4145e8040e2f088906be71c18eb7b93e
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-11 15:01:01 +0400

    Add Object lifecycle

--- ClinicAppointmentTask/Program.cs
+++ ClinicAppointmentTask/Program.cs
@@ -1,4 +1,6 @@
 
+using ClinicAppointmentTask.Repositories;
+using ClinicAppointmentTask.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace ClinicAppointmentTask
@@ -11,7 +13,13 @@ namespace ClinicAppointmentTask
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Add services to the container.
+            builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+            builder.Services.AddScoped<IClinicRepository, ClinicRepository>();
+            builder.Services.AddScoped<IBookingRepository, BookingRepository>();
 
+            builder.Services.AddScoped<IPatientService, PatientService>();
+            builder.Services.AddScoped<IClinicService, ClinicService>();
+            builder.Services.AddScoped<IBookingService, BookingService>();
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
