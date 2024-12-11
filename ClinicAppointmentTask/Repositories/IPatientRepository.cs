972823e2d77a5c1d1b6cd14c2c0100a824d3f2e9
Tasnim <tasnimalbrashdi@gmail.com>
2024-12-11 09:34:41 +0400

    Add interface Pation Repository

---
+++ ClinicAppointmentTask/Repositories/IPatientRepository.cs
@@ -0,0 +1,10 @@
+﻿using ClinicAppointmentTask.Models;
+
+namespace ClinicAppointmentTask.Repositories
+{
+    public interface IPatientRepository
+    {
+        string Add(Patient patient);
+        IEnumerable<Patient> GetAll();
+    }
+}
